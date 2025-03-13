34f80f11c1ec4b03c9fa50335ba2c86f227227a5
charleskemp1975622 <CharlesKemp1975622@hotmail.com>
2025-03-13 10:51:45 -0500

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./ui/**/*.{js,ts,jsx,tsx,mdx}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["winter"],
+  },
+};
